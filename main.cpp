1a196401eb7705ca724b6a29641643f07d143013
Dappstr <87837549+Dappstr@users.noreply.github.com>
2022-01-14 17:36:52 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <ctime>
+
+int makeRand()
+{
+	srand(time(NULL));
+	return (int)rand() % 100 + 1;
+}
+
+class Computer
+{
+private:
+	static int randNum;
+
+public:
+	static int num;
+	static int generateNum()
+	{
+		num = randNum;
+		return num;
+	}
+
+};
+int Computer::randNum = makeRand();
+int Computer::num {};
+
+auto playGame (int guessedNum, int generatedNum) -> bool
+{
+	while (guessedNum != generatedNum)
+	{
+		std::cin >> guessedNum;
+		if(guessedNum < generatedNum)
+		{
+			std::cout << "Lower than generated number!\n";
+		}
+		else if (guessedNum > generatedNum)
+		{
+			std::cout << "Higher than generated number!\n";
+		}
+	}
+	std::cout << "You win!\n";
+	return true;
+}
+
+int main()
+{
+	int guessedNum {};
+	int generatedNum {Computer::generateNum()};
+
+	bool gameSwitch (true);
+	while (gameSwitch)
+	{
+		std::cout << "Enter guess: ";
+		playGame(guessedNum, generatedNum);
+		gameSwitch = false;
+	}
+}
