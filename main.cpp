e9e4eb7689de91e67093306003c350ce8277aa60
Lane <87837549+Dappstr@users.noreply.github.com>
2022-03-07 19:26:51 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,57 +1,102 @@
 #include <iostream>
 #include <ctime>
-
-int makeRand()
-{
-	srand(time(NULL));
-	return (int)rand() % 100 + 1;
-}
+#include <string>
 
 class Computer
 {
 private:
-	static int randNum;
+	int m_randNum {};
 
 public:
-	static int num;
-	static int generateNum()
+	void setRandNum();
+
+	int getComputerNum() const
 	{
-		num = randNum;
-		return num;
+		return m_randNum;
 	}
 
 };
-int Computer::randNum = makeRand();
-int Computer::num {};
 
-auto playGame (int& guessedNum, int& generatedNum) -> bool
+void Computer::setRandNum()
 {
-	while (guessedNum != generatedNum)
+	srand(time(NULL));
+	int num{ rand() % 100 + 1 };
+
+	this->m_randNum = num;
+}
+
+auto playGame(std::string& input, const int& generatedNum) -> bool
+{
+	std::cout << "Enter 'e' to exit\n";
+	int inputToGuess {};
+
+	do
 	{
-		std::cout << "Enter guess: ";
-		std::cin >> guessedNum;
-		if(guessedNum < generatedNum)
+		std::cout << ">> ";
+		std::cin >> input;
+		{
+			if (input == "e" || input == "E")
+			{
+				std::cout << "Exiting.\n";
+				return false;
+			}
+			try
+			{
+				inputToGuess = std::stoi(input);
+			}
+
+			catch (std::exception& e)
+			{
+				std::cout << "Input misunderstood: " << e.what() << '\n';
+			}
+		}
+
+		if (inputToGuess < generatedNum)
 		{
 			std::cout << "Lower than generated number!\n";
 		}
-		else if (guessedNum > generatedNum)
+
+		else if (inputToGuess > generatedNum)
 		{
 			std::cout << "Higher than generated number!\n";
 		}
-	}
+
+	} while (inputToGuess != generatedNum);
+
 	std::cout << "You win!\n";
 	return true;
 }
 
 int main()
 {
-	int guessedNum {};
-	int generatedNum {Computer::generateNum()};
+	std::string input    {};
+	Computer computerObj {};
 
-	bool gameSwitch (true);
+	bool gameSwitch(true);
+
 	while (gameSwitch)
 	{
-		playGame(guessedNum, generatedNum);
-		gameSwitch = false;
+		computerObj.setRandNum();
+		int randNum{ computerObj.getComputerNum()};
+
+		playGame(input, randNum);
+
+		std::cout << "Continue playing? (y/n)";
+		std::cin >> input;
+
+		if (input == "y" || input == "Y")
+		{
+			computerObj.setRandNum();
+		}
+
+		else if(input == "n" || input == "N")
+		{
+			gameSwitch = false;
+		}
+
+		else
+		{
+			std::cout << "Input not recognized. Continuing anyway.\n";
+		}
 	}
 }
